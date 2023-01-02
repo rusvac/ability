e997b3b826c55dce7356b35a07cb559c64a8c885
Judith <rvacanti@gmail.com>
2023-01-02 00:33:23 +0000

    Create sample page

---
+++ src/pages/samp.tsx
@@ -0,0 +1,20 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Box,
+  Code,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/react";
+import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+
+import { Container } from "../components/skel/Container";
+
+const Samples = () => (
+  <Container height="100vh">
+    <Box></Box>
+  </Container>
+);
+
+export default Samples;
