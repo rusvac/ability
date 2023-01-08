5b1469ba78120e6e1bdfeb7b8a646eaf4fa71c12
Judith <rvacanti@gmail.com>
2023-01-08 22:53:05 +0000

    Update sample page

--- src/pages/samp.tsx
+++ src/pages/samp.tsx
@@ -2,50 +2,115 @@ import {
   Link as ChakraLink,
   Text,
   Box,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
   Flex,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Textarea,
+  Button,
   useColorModeValue as mode,
+  useTab,
+  useMultiStyleConfig,
 } from "@chakra-ui/react";
-import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+import { SmallCloseIcon } from "@chakra-ui/icons";
 
 import { Container } from "../components/skel/Container";
 import ContractABI from "../components/ContractABI";
 
-import { abis } from "@/lib/abis";
+import React, { useState } from "react";
+import SelectABI from "@/comp/SelectABI";
 
-const Samples = () => (
-  <Container height="100vh" overflow={"hidden"}>
-    <Box w="full">
-      <Box w="full">
-        <Text>Sample Contract and UI</Text>
+const CustomTab = React.forwardRef((props: any, ref) => {
+  // 1. Reuse the `useTab` hook
+  const tabProps = useTab({ ...props, ref });
+  const isSelected = !!tabProps["aria-selected"];
+
+  // 2. Hook into the Tabs `size`, `variant`, props
+  const styles = useMultiStyleConfig("Tabs", tabProps);
+
+  const normalizedProps: any = { ...tabProps };
+
+  return (
+    <Button
+      {...tabProps}
+      size="sm"
+      fontWeight="normal"
+      rounded="none"
+      py={1}
+      bg={mode("gray.200", "gray.700")}
+      _hover={{
+        bg: mode("gray.200", "gray.600"),
+      }}
+      _selected={{
+        bg: mode("gray.200", "gray.600"),
+      }}
+    >
+      {tabProps.children}
+      {isSelected && normalizedProps.onClear ? (
+        <Box px={1} onClick={normalizedProps.onClear}>
+          <SmallCloseIcon />
+        </Box>
+      ) : (
+        <></>
+      )}
+    </Button>
+  );
+});
+
+const Samples = () => {
+  const [tabIndex, setTabIndex] = useState<number>(0);
+  const [tabs, setTabs] = useState<any[]>([]);
+  const [abi, setABI] = useState<any>(undefined);
+
+  const handleTabsChange = (index) => {
+    setTabIndex(index);
+  };
+
+  const addTab = (ABI) => {
+    setTabs([...tabs, { title: ABI.id, content: ABI }]);
+    handleTabsChange(tabs.length + 1);
+  };
+
+  const closeTab = (id) => {
+    if (tabs.length - 1 == id) {
+      handleTabsChange(0);
+    }
+    setTabs([...tabs.filter((el, i) => id != i)]);
+  };
+
+  return (
+    <Container>
+      <Box w="full" h="full">
+        <Tabs
+          variant="soft-rounded"
+          colorScheme="gray"
+          index={tabIndex}
+          onChange={handleTabsChange}
+        >
+          <TabList borderBottomWidth={1}>
+            <CustomTab>ABILITY+</CustomTab>
+            {tabs.map((el, i) => (
+              <CustomTab key={i} onClear={() => closeTab(i)}>
+                {el.title}
+              </CustomTab>
+            ))}
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <SelectABI addTab={addTab} />
+            </TabPanel>
+            {tabs.map((el, i) => (
+              <TabPanel key={i} p={0}>
+                <ContractABI abi={el.content} />
+              </TabPanel>
+            ))}
+          </TabPanels>
+        </Tabs>
       </Box>
-
-      <Flex
-        direction={"column"}
-        overflowY="scroll"
-        height="100%"
-        minH="px"
-        maxH="full"
-        sx={{
-          "&::-webkit-scrollbar-track": {
-            bg: "transparent",
-          },
-          "&::-webkit-scrollbar": {
-            width: "4px",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            bg: mode("blue.600", "gray.700"),
-            borderRadius: "20px",
-          },
-        }}
-      >
-        <ContractABI abi={abis[0]} />
-      </Flex>
-    </Box>
-  </Container>
-);
+    </Container>
+  );
+};
 
 export default Samples;
